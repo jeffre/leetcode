c41fe84d9ee2517db005f878ab0ed696cd4ad8dc
Jeff Guymon <jeffre@users.noreply.github.com>
2022-07-05 19:44:53 -0700

    refactored

--- 31/nextPermutation.go
+++ 31/nextPermutation.go
@@ -5,51 +5,32 @@ import (
 )
 
 func nextPermutation(nums []int) {
-	length := len(nums)
 
-	// Early return edge cases
-	if length <= 1 {
-		return
-	}
+	// Iterate backwards through nums
+	for i := len(nums) - 1; i > 0; i-- {
 
-	// Iterate backwards through nums to find the index (idx) that needs to be
-	// increased
-	idx := length - 2
-	for ; idx > -1; idx-- {
-		if nums[idx] < nums[idx+1] {
-			break
+		// Skip until left-neighbor is smaller
+		if nums[i] <= nums[i-1] {
+			continue
 		}
-	}
 
-	// Edge cases
-	switch idx {
-	case -1:
-		// No more permutations are possible; Restart
-		sort.IntSlice(nums).Sort()
-		return
-
-	case length - 2:
-		// Last two nums can be swapped
-		nums[idx], nums[idx+1] = nums[idx+1], nums[idx]
-		return
-	}
-
-	// At this point we know nums[idx+2] exists
-
-	// Of the values to the right of idx, find the index of value that is
-	// larger than num[idx] but smaller than the rest
-	minIndex := idx + 1
-	minNum := nums[minIndex]
-	for i := idx + 2; i < length; i++ {
-		if nums[i] < minNum && nums[i] > nums[idx] {
-			minNum = nums[i]
-			minIndex = i
+		// Find the index of the first value larger than num[i] (j). Since the
+		// previous step established that the right side of i is sorted this
+		// is simple:
+		j := len(nums) - 1
+		for nums[j] <= nums[i-1] {
+			j--
+			continue
 		}
+
+		// Swap i-1 and j values
+		nums[i-1], nums[j] = nums[j], nums[i-1]
+
+		// Sort everything to the right of index
+		sort.Ints(nums[i:])
+
+		return
 	}
 
-	// Swap idx and minIndex values
-	nums[idx], nums[minIndex] = nums[minIndex], nums[idx]
-
-	// Sort everything to the right of index in ascending order
-	sort.IntSlice(nums[idx+1:]).Sort()
+	sort.Ints(nums)
 }
