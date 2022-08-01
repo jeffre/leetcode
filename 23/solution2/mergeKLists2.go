02709f954991c90555052fe9f93b27578b0bfb72
jeffre <jeffre@users.noreply.github.com>
2022-08-01 15:54:47 -0700

    solved by comparison

---
+++ 23/solution2/mergeKLists2.go
@@ -0,0 +1,57 @@
+package leetcode23s2
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 5509 ns/op
+func mergeKLists(lists []*ListNode) *ListNode {
+	sentinel := &ListNode{0, nil}
+	helper(lists, sentinel)
+	return sentinel.Next
+}
+
+func helper(lists []*ListNode, result *ListNode) {
+
+	// Remove nilled ListNode from 'lists'
+	for i := 0; i < len(lists); {
+		if lists[i] != nil {
+			i++
+			continue
+		}
+
+		if i < len(lists)-1 {
+			copy(lists[i:], lists[i+1:])
+		}
+
+		lists[len(lists)-1] = nil
+		lists = lists[:len(lists)-1]
+	}
+
+	switch len(lists) {
+	case 0:
+		// All lists have been fully processed
+		return
+	case 1:
+		// All remaining nodes are in the same list so we can inherit the
+		// parent and be done
+		result.Next = lists[0]
+		return
+	}
+
+	// Find which list has the lowest value
+	lowest := 0
+	for i := 1; i < len(lists); i++ {
+		if lists[i].Val < lists[lowest].Val {
+			lowest = i
+		}
+	}
+
+	// Add lowest value to solution and advance that list
+	result.Next = &ListNode{lists[lowest].Val, nil}
+	result = result.Next
+	lists[lowest] = lists[lowest].Next
+
+	helper(lists, result)
+}
---
+++ 23/solution2/mergeKLists2_test.go
@@ -0,0 +1,147 @@
+package leetcode23s2
+
+import (
+	"fmt"
+	"testing"
+)
+
+type test struct {
+	name  string
+	given []*ListNode
+	want  *ListNode
+}
+
+func getTests() []test {
+
+	return []test{
+		{
+			name:  "0 nodes",
+			given: []*ListNode{},
+			want:  nil,
+		}, {
+			name: "1 listnode",
+			given: []*ListNode{
+				intsToList([]int{1, 4, 5}),
+			},
+			want: intsToList([]int{1, 4, 5}),
+		}, {
+			name: "3 listnodes",
+			given: []*ListNode{
+				nil,
+				nil,
+				intsToList([]int{1, 4, 5}),
+			},
+			want: intsToList([]int{1, 4, 5}),
+		}, {
+			name: "simple-merge",
+			given: []*ListNode{
+				nil,
+				intsToList([]int{6}),
+				intsToList([]int{1, 4, 5}),
+			},
+			want: intsToList([]int{1, 4, 5, 6}),
+		}, {
+			name: "triple-merge",
+			given: []*ListNode{
+				intsToList([]int{1, 4, 5}),
+				intsToList([]int{1, 3, 4}),
+				intsToList([]int{2, 6}),
+			},
+			want: intsToList([]int{1, 1, 2, 3, 4, 4, 5, 6}),
+		}, {
+			name: "large-merge",
+			given: []*ListNode{
+				intsToList([]int{1, 4, 5}),
+				intsToList([]int{1, 3, 4}),
+				intsToList([]int{2, 6, 12, 33, 45}),
+				intsToList([]int{2, 61, 63, 64, 66}),
+				intsToList([]int{2, 6, 675, 676, 6657}),
+				intsToList([]int{2, 6, 15, 17, 17}),
+			},
+			want: intsToList([]int{1, 1, 2, 2, 2, 2, 3, 4, 4, 5, 6, 6, 6, 12, 15,
+				17, 17, 33, 45, 61, 63, 64, 66, 675, 676, 6657}),
+		}, {
+			name: "negative-vals",
+			given: []*ListNode{
+				intsToList([]int{-1, 1}),
+				intsToList([]int{-3, 1, 4}),
+				intsToList([]int{-2, -1, 0, 2}),
+			},
+			want: intsToList([]int{-3, -2, -1, -1, 0, 1, 1, 2, 4}),
+		},
+	}
+}
+
+func TestCases(t *testing.T) {
+	for _, tt := range getTests() {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got := mergeKLists(tt.given)
+			if !assertIdenticalListNode(t, got, tt.want) {
+				t.Errorf("\ngiven: %v\ngot:   %v\nwant:  %v\n",
+					listsToInts(tt.given), listToInts(got), listToInts(tt.want))
+			}
+		})
+	}
+}
+
+func BenchmarkCases(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		for _, tt := range getTests() {
+			mergeKLists(tt.given)
+		}
+	}
+}
+
+func assertIdenticalListNode(t testing.TB, a, b *ListNode) bool {
+	t.Helper()
+	for a != nil && b != nil {
+		if a.Val != b.Val {
+			return false
+		}
+		a = a.Next
+		b = b.Next
+	}
+	return a == nil && b == nil
+}
+
+func listsToInts(nodes []*ListNode) [][]int {
+	result := make([][]int, len(nodes))
+
+	for i, n := range nodes {
+		result[i] = listToInts(n)
+	}
+	return result
+}
+
+func listToInts(head *ListNode) (result []int) {
+	limit := 100
+
+	for head != nil {
+		limit--
+		if limit < 0 {
+			msg := fmt.Sprintf("The chain depth exceeds %d. This may be the "+
+				"result of an infinite loop.", limit)
+			panic(msg)
+		}
+
+		result = append(result, head.Val)
+		head = head.Next
+	}
+
+	return result
+}
+
+func intsToList(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	sentinel := &ListNode{}
+	cur := sentinel
+	for _, v := range nums {
+		cur.Next = &ListNode{Val: v}
+		cur = cur.Next
+	}
+	return sentinel.Next
+}
