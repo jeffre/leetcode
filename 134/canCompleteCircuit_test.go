79bd186d1023f7e457a63336b24a379ff68dd41c
jeffre <jeffre@users.noreply.github.com>
2022-08-15 11:54:17 -0700

    DRY testing and benchmark

--- 134/canCompleteCircuit_test.go
+++ 134/canCompleteCircuit_test.go
@@ -7,32 +7,43 @@ type given struct {
 	cost []int
 }
 
-var cases = []struct {
-	name  string
-	given given
-	want  int
-}{
+type want struct {
+	int
+}
+
+func runTest(given given) want {
+	got := canCompleteCircuit(given.gas, given.cost)
+	return want{got}
+}
+
+var tests = []test{
 	{
-		name: "example1",
-		given: given{
+		"example1",
+		given{
 			gas:  []int{1, 2, 3, 4, 5},
 			cost: []int{3, 4, 5, 1, 2},
 		},
-		want: 3,
+		want{3},
 	}, {
-		name: "example2",
-		given: given{
+		"example2",
+		given{
 			gas:  []int{2, 3, 4},
 			cost: []int{3, 4, 3},
 		},
-		want: -1,
+		want{-1},
 	},
 }
 
+type test struct {
+	name  string
+	given given
+	want  want
+}
+
 func TestCases(t *testing.T) {
-	for _, tt := range cases {
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := canCompleteCircuit(tt.given.gas, tt.given.cost)
+			got := runTest(tt.given)
 			if got != tt.want {
 				t.Errorf("\ngot:   %#v\nwant:  %#v\n", got, tt.want)
 			}
@@ -42,8 +53,8 @@ func TestCases(t *testing.T) {
 
 func BenchmarkCases(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		for _, tt := range cases {
-			canCompleteCircuit(tt.given.gas, tt.given.cost)
+		for _, tt := range tests {
+			runTest(tt.given)
 		}
 	}
 }
